b292cef54f8e810f3fcce59a8484839be0cc4419
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-23 18:38:12 +0800

    AM

---
+++ src/main/java/com/itheima/spring/bean/impl/Anime01.java
@@ -0,0 +1,18 @@
+package com.itheima.spring.bean.impl;
+
+import com.itheima.spring.bean.Anime;
+
+/**
+ * Description==>TODO
+ * BelongsProject==>spring_basic01
+ * BelongsPackage==>com.itheima.spring.bean.impl
+ * CreateTime==>2022-09-23 18:34:48
+ * Version==>1.0
+ * Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
+public class Anime01 implements Anime {
+    @Override
+    public void printAnimeName() {
+        System.out.println(" >> 国王排名...");
+    }
+}
