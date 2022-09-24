ac6e5866d61c3440f8f1fc78bafa97606689328e
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-24 17:42:01 +0800

    AM

--- src/main/java/com/itheima/spring/service/AnimeTest01.java
+++ src/main/java/com/itheima/spring/service/AnimeTest01.java
@@ -1,6 +1,9 @@
 package com.itheima.spring.service;
 
+import com.itheima.spring.dao.AnimeTime;
 import com.itheima.spring.impl.AnimeInfoImpl;
+import com.itheima.spring.impl.AnimeNameImpl;
+import com.itheima.spring.impl.AnimeTimeImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,13 +19,42 @@ import java.util.Scanner;
  */
 public class AnimeTest01 {
 
+    public static ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext(
+            "applicationContext.xml");
+
+
+    @Test
+    public void testPrototype(){
+
+        AnimeNameImpl bean01 = cpx.getBean(AnimeNameImpl.class);
+        AnimeNameImpl bean02 = cpx.getBean(AnimeNameImpl.class);
+
+//        System.out.println(bean.getAnimeName());
+
+        System.out.println(bean01);
+        System.out.println(bean02);
+
+    }
+
+    @Test
+    public void testSingleTon(){
+
+        AnimeTimeImpl animeTime01 = (AnimeTimeImpl) cpx.getBean("AnimeTimeImpl");
+        AnimeTimeImpl animeTime02 = (AnimeTimeImpl) cpx.getBean("AnimeTimeImpl");
+
+        System.out.println("01 "+animeTime01);
+        System.out.println("02 "+animeTime02);
+
+
+    }
+
+
+
+
 
 
     @Test
     public void getBean(){
-
-        ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
 
         AnimeInfoImpl animeInfo = (AnimeInfoImpl) cpx.getBean("AnimeInfoImpl");
 
